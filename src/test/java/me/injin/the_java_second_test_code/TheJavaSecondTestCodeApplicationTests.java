098b19fd72ebe4bc7d6d3bd86f2fe781ddbcc22a
injinjeong <snowwwwww@naver.com>
2022-09-03 10:07:20 +0900

    git init commit

---
+++ src/test/java/me/injin/the_java_second_test_code/TheJavaSecondTestCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.injin.the_java_second_test_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheJavaSecondTestCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
