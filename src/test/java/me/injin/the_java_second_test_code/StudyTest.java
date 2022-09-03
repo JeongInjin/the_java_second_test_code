025e5ddb3b6a4ec65e4cad4148ad8d3bed28a596
injinjeong <snowwwwww@naver.com>
2022-09-03 16:33:54 +0900

    코드조건, annotaion 조건으로 테스트 설정

--- src/test/java/me/injin/the_java_second_test_code/StudyTest.java
+++ src/test/java/me/injin/the_java_second_test_code/StudyTest.java
@@ -1,34 +1,43 @@
 package me.injin.the_java_second_test_code;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.time.Duration;
-
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
+    private final static Logger log = LogManager.getLogger(StudyTest.class);
 
-    @BeforeAll
-    static void beforeAll() {
-        System.out.println("static 메서드여야하고 반환값이 없어야함, 테스트 시작 전 1번만 실행 beforeAll");
-    }
-
-    @AfterAll
-    static void AfterAll() {
-        System.out.println("static 메서드여야하고 반환값이 없어야함, 테스트 시작 후 1번만 실행 AfterAll");
-    }
-
-    @BeforeEach
-    public void BeforeEach() {
-        System.out.println("테스트 시작 전 실행 -> BeforeEach");
-    }
-
-    @AfterEach
-    public void AfterEach() {
-        System.out.println("테스트 종료 후 실행 -> AfterEach");
-    }
+//    @BeforeAll
+//    static void beforeAll() {
+//        System.out.println("static 메서드여야하고 반환값이 없어야함, 테스트 시작 전 1번만 실행 beforeAll");
+//    }
+//
+//    @AfterAll
+//    static void AfterAll() {
+//        System.out.println("static 메서드여야하고 반환값이 없어야함, 테스트 시작 후 1번만 실행 AfterAll");
+//    }
+//
+//    @BeforeEach
+//    public void BeforeEach() {
+//        System.out.println("테스트 시작 전 실행 -> BeforeEach");
+//    }
+//
+//    @AfterEach
+//    public void AfterEach() {
+//        System.out.println("테스트 종료 후 실행 -> AfterEach");
+//    }
 
     @Test
     @DisplayName("스터디 만들기 \uD83D\uDE0E")
@@ -69,6 +78,60 @@ class StudyTest {
             Thread.sleep(50);
         });
     }
+
+    @Test
+    @DisplayName("코드 조건에 따른 테스트.")
+    public void study_condition_test() {
+        String test_value = "ok";
+        assumeTrue("ok".equalsIgnoreCase(test_value)); //해당 조건이 true 인경우 진행한다.
+
+        Study study = new Study(10);
+        assertThat(study.getLimit()).isGreaterThan(0);
+        log.info("조건 true 로 인하여 테스트 완료.");
+
+        /**
+         * assumingThat(boolean, executable)
+         * 첫 번째 인자가 True면 두 번째 인자로 들어온 함수 실행
+         * 첫 번째 인자 값이 false 인 경우에도 테스트를 스킵하지 않고 다음 코드를 진행합니다.
+         */
+        assumingThat("not_ok".equalsIgnoreCase(test_value), () -> {
+            assertThat(study.getLimit()).isGreaterThan(0);
+            log.info("assumingThat false");
+        });
+
+        assumingThat("ok".equalsIgnoreCase(test_value), () -> {
+            assertThat(study.getLimit()).isGreaterThan(0);
+            log.info("assumingThat true");
+        });
+
+    }
+
+    /**
+     * Annotaion 조건에 따른 설정으로 테스트를 추가, 제외 시킬 수 있다.
+     * https://effortguy.tistory.com/121
+     */
+    @Test
+    @DisplayName("환경 변수 조건에 따른 테스트.")
+    @EnabledIfEnvironmentVariable(named = "ENV", matches = "local")
+    public void EnabledIfEnvironmentVariableLocal() {
+        log.info("EnabledIfEnvironmentVariable: ENV: local");
+    }
+
+    @Test
+    @DisplayName("Annotaion Enabled OS 조건에 따른 테스트.")
+//    @EnabledOnOs(OS.MAC)
+    @EnabledOnOs({OS.MAC, OS.SOLARIS})
+    public void EnabledOnOsMAC() {
+        log.info("EnabledOnOs: OS.MAC 또는 OS.SOLARIS 입니다.");
+    }
+
+    @Test
+    @DisplayName("Annotaion Disabled OS 조건에 따른 테스트.")
+    @DisabledOnOs({OS.WINDOWS, OS.LINUX})
+    public void EnabledOnOsWINDOWS() {
+        log.info("DisabledOnOs: OS.WINDOWS, OS.LINUX 가 아닙니다.");
+    }
+
 
     @Test
     @Disabled
