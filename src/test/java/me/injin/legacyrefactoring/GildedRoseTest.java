1789b64be5c70fc2921267d4a67e6ab04c2c2e7d
injinjeong <snowwwwww@naver.com>
2022-11-11 13:48:19 +0900

    test data 추가로 모든 경우의 수 100프로 맞춤

--- src/test/java/me/injin/legacyrefactoring/GildedRoseTest.java
+++ src/test/java/me/injin/legacyrefactoring/GildedRoseTest.java
@@ -12,7 +12,7 @@ class GildedRoseTest {
     @Test
     void updateQuality() {
         String[] name = {"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"};
-        Integer[] sellIn = {-1, 0, 11};
+        Integer[] sellIn = {-1, 0, 1, 6, 11};
         Integer[] quality = {0, 1, 49, 50};
 
         //여러 조합으로 테스트를 해야할 시
