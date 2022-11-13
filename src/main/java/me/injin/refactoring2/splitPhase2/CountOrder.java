5b21e4519b761a1d0b0d0e148ca7c5ccaf13a4ed
injinjeong <snowwwwww@naver.com>
2022-11-13 11:29:22 +0900

    -refactor splitPhase2 추가

---
+++ src/main/java/me/injin/refactoring2/splitPhase2/CountOrder.java
@@ -0,0 +1,32 @@
+package me.injin.refactoring2.splitPhase2;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+public class CountOrder {
+    private record Order(String status) {}
+
+    public static void main(String[] args) {
+        try {
+            if(args.length == 0)
+                throw new RuntimeException("파일명을 입력하세요");
+            String filename = args[args.length - 1];
+            File input = Paths.get(filename).toFile();
+            ObjectMapper mapper = new ObjectMapper();
+            Order[] orders = mapper.readValue(input, Order[].class);
+            if (Arrays.asList(args).contains("-r")) {
+                System.out.println(Arrays.stream(orders)
+                        .filter(o -> "ready".equals(o.status()))
+                        .count());
+            } else {
+                System.out.println(orders.length);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            System.exit(1);
+        }
+    }
+}
